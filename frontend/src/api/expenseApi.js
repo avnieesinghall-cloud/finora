a1b7c4ffdb6d351139db05cb708277c668d20e2b
Avni <avnieesinghall@gmail.com>
2026-05-25 16:12:56 +0530

    Fix production API base URL

--- frontend/src/api/expenseApi.js
+++ frontend/src/api/expenseApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: "https://finora-backend-ons2.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
